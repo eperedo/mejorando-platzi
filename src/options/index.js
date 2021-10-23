1c0e5261e196bbdc6c9e4307ff6142522bae156c
Eduardo P. Rivero <jrperedo@gmail.com>
2021-10-23 18:49:34 -0500

    feat: Add translations for options page

--- src/options/index.js
+++ src/options/index.js
@@ -1,3 +1,26 @@
+// locales
+const TR_KEYS = {
+	optionsButtonSave: 'optionsButtonSave',
+	optionsTitle: 'optionsTitle',
+	optionsLightMode: 'optionsLightMode',
+	optionsStories: 'optionsStories',
+	optionsXRay: 'optionsXRay',
+};
+const TR_TITLE = chrome.i18n.getMessage(TR_KEYS.optionsTitle);
+const TR_LIGHT_MODE = chrome.i18n.getMessage(TR_KEYS.optionsLightMode);
+const TR_STORIES = chrome.i18n.getMessage(TR_KEYS.optionsStories);
+const TR_XRAY = chrome.i18n.getMessage(TR_KEYS.optionsXRay);
+const TR_BUTTON_SAVE = chrome.i18n.getMessage(TR_KEYS.optionsButtonSave);
+window.document.title = TR_TITLE;
+document.querySelector('h1#option-title').textContent = TR_TITLE;
+document.querySelector(`label#${TR_KEYS.optionsLightMode}`).textContent =
+	TR_LIGHT_MODE;
+document.querySelector(`label#${TR_KEYS.optionsStories}`).textContent =
+	TR_STORIES;
+document.querySelector(`label#${TR_KEYS.optionsXRay}`).textContent = TR_XRAY;
+document.querySelector(`button#${TR_KEYS.optionsButtonSave}`).textContent =
+	TR_BUTTON_SAVE;
+
 const form = document.querySelector('form#form-options');
 const btn = document.querySelector('button.btn');
 const lightCourseDom = form.querySelector('input#enableLightMode');
