77645cf7dd825c3c09fb8a92d84bee3c064f42c4
Eduardo P. Rivero <jrperedo@gmail.com>
2021-10-23 17:05:20 -0500

    feat: Added time remaining in each course

--- src/features/tutorial-pagination/tutorial.js
+++ src/features/tutorial-pagination/tutorial.js
@@ -1,6 +1,8 @@
 const tutorials = window.initialData?.initialState?.tutorials;
+const course = window.initialData?.initialState?.course;
 const courseId = window.initialData?.initialState?.course?.id;
 const userId = window.contactInfo?.userId;
+const concepts = window.initialData?.initialState?.concepts;
 
 const avatarDom = document.querySelector('.Actionsv2-avatar');
 let avatarUrl = '';
@@ -75,10 +77,43 @@ function renderTutorials(data = []) {
 	});
 }
 
+function addLaunchDate(launch_date) {
+	if (launch_date) {
+		const $el = document.querySelector('.AddToLearningPath');
+		const $parent = $el.parentNode;
+		const $dom = document.createElement('p');
+		$dom.classList.add('mp-launch-date');
+		$dom.setAttribute('title', launch_date);
+		$dom.textContent = `Curso lanzado el ${new Date(
+			launch_date,
+		).toLocaleDateString()}`;
+		$parent.insertBefore($dom, $el);
+	}
+}
+
+function setQuantityOfTutorials(numberTutorials, numberOfPages) {
+	if (numberTutorials > 0) {
+		const tabs = document.querySelectorAll('.SingleTab');
+		let $tutorialTab;
+		tabs.forEach((tab) => {
+			if (tab.textContent.toLowerCase() === 'tutoriales de estudiantes') {
+				$tutorialTab = tab;
+			}
+		});
+		let totalNumber = numberTutorials;
+		if (numberOfPages > 1) {
+			totalNumber = (numberOfPages - 1) * numberTutorials;
+		}
+		$tutorialTab.textContent +=
+			numberOfPages > 1 ? ` (+${totalNumber})` : ` (${totalNumber})`;
+	}
+}
+
 if (tutorials) {
 	const tabContentDom = document.querySelector('div.TabsContent-elements');
 	if (tabContentDom) {
 		const totalPages = tutorials.pagesCount;
+		setQuantityOfTutorials(tutorials.results.length, tutorials.pagesCount);
 		if (totalPages > 1) {
 			const pageDom = document.createElement('div');
 			pageDom.classList.add('TutorialsPages');
@@ -109,4 +144,112 @@ if (tutorials) {
 			tabContentDom.appendChild(pageDom);
 		}
 	}
+}
+
+if (course) {
+	addLaunchDate(course.launch_date);
+}
+
+if (concepts) {
+	const $tabClasses = document.querySelectorAll('.SingleTab')[0];
+
+	function pluralize(text, number) {
+		return number > 1 ? `${text}s` : text;
+	}
+
+	function renderClassInfo(addGlobalProgress) {
+		const $titleEls = document.querySelectorAll('.Material-title');
+		let totalSeenInMinutes = 0;
+		let totalInMinutes = 0;
+		const courseInfo = concepts.reduce((acumParent, item, index) => {
+			const result = item.materials.reduce(
+				(acum, material) => {
+					const minutes = Math.floor(material.duration / 60);
+					const seconds = material.duration % 60;
+					acum.totalSeconds += seconds;
+					acum.totalMinutes += minutes;
+					if (material.seen) {
+						acum.totalSecondsSeen += seconds;
+						acum.totalMinutesSeen += minutes;
+					}
+					return acum;
+				},
+				{
+					totalSeconds: 0,
+					totalSecondsSeen: 0,
+					totalMinutes: 0,
+					totalMinutesSeen: 0,
+				},
+			);
+
+			const quantityClasses = item.materials.length;
+			const labelClases = pluralize('clase', item.materials.length);
+			const hoursTotal = Math.floor(result.totalMinutes / 60);
+			const secsToMinutes = Math.floor((result.totalSecondsSeen / 60) % 60);
+			const minutesTotal = result.totalMinutes + secsToMinutes;
+			let description = `${item.title} - ${quantityClasses} ${labelClases} en `;
+			if (hoursTotal > 0) {
+				description += `${hoursTotal} ${pluralize('hora', hoursTotal)} `;
+			}
+			if (minutesTotal > 0) {
+				if (hoursTotal > 0) {
+					description += `y `;
+				}
+				description += `${minutesTotal} ${pluralize('minuto', minutesTotal)}`;
+			}
+
+			if ($titleEls[index]) {
+				$titleEls[index].textContent = description;
+			}
+
+			// total seen
+			totalSeenInMinutes += result.totalMinutesSeen;
+			totalInMinutes += result.totalMinutes;
+
+			acumParent.push({
+				title: item.title,
+				description,
+				quantityClasses: item.materials.length,
+				...result,
+			});
+			return acumParent;
+		}, []);
+		if (addGlobalProgress) {
+			const $progress = document.querySelector(
+				'.CourseDetail-middle-right span',
+			);
+			if ($progress) {
+				const diffTime = totalInMinutes - totalSeenInMinutes;
+				if (diffTime > 0) {
+					const hours = Math.floor(diffTime / 60);
+					if (hours > 0) {
+						$progress.textContent += ` - ${hours} ${pluralize(
+							'hora',
+							hours,
+						)} restantes.`;
+					} else {
+						$progress.textContent += ` - ${diffTime} ${pluralize(
+							'minuto',
+							diffTime,
+						)} restantes.`;
+					}
+				}
+				window.totalTime = {
+					totalSeenInMinutes,
+					totalInMinutes,
+				};
+			}
+		}
+		window.final = courseInfo;
+	}
+
+	if ($tabClasses) {
+		$tabClasses.addEventListener('click', () => {
+			setTimeout(() => {
+				renderClassInfo(false);
+			}, 1000);
+		});
+	}
+
+	renderClassInfo(true);
 }
